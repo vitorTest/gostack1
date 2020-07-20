768f202c2a826637e7ed26cec01be4d0920063de
vitor <vytorfelix2@gmail.com>
2020-07-20 12:55:30 -0300

    modulo2 part 1

--- modulo1/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const server = express();
-
-server.get('/teste', (req, res) => {
-    return res.json({message: 'hello new friend'});
-});
-
-server.listen(3000);
