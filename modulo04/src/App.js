8f032600e0e54323f97a4b1694e019f7e41c3bd1
vitor <vytorfelix2@gmail.com>
2020-08-11 13:20:48 -0300

    Initiating React - config

---
+++ modulo04/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+import manAtLobby from './assets/manAtLobby.png'
+
+function App(){
+  return <img width="200" src={manAtLobby} />
+}
+
+export default App;
