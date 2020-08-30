3b3655d1cc2c34e6dbb221ae9eaae56726d7ec25
vitor <vytorfelix2@gmail.com>
2020-08-30 18:19:42 -0300

    refactor: set tag li to work in a component

---
+++ modulo04/src/components/TechItem.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function TechItem({ tech, onDelete }) {
+  return (
+    <li>
+      {tech}
+      <button 
+        onClick={onDelete}
+        type="button"
+        >Remover
+      </button>
+    </li>
+  );
+}
+
+export default TechItem;
--- modulo04/src/components/TechList.js
+++ modulo04/src/components/TechList.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import TechItem from './TechItem';
+
 class TechList extends Component {
   state = {
     newTech: '',
@@ -31,14 +33,12 @@ class TechList extends Component {
     return (
       <form onSubmit={this.handleSubmit}>
         <ul>
-          {this.state.techs.map(tech => (
-            <li key={tech}>
-              {tech}
-              <button 
-                onClick={() => this.handleDelete(tech)}
-                type="button">Remover</button>
-            </li>
-          ))}      
+          {this.state.techs.map(tech => 
+            <TechItem 
+              key={tech} 
+              tech={tech} 
+              onDelete={() => this.handleDelete(tech)}
+            />)}      
         </ul>
         <input 
           type="text" 
