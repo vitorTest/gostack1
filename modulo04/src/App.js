956dae15454f8c1af2dad2270bc762a934374bbc
vitor <vytorfelix2@gmail.com>
2020-08-30 00:10:04 -0300

    feat: add & remove list's itens

--- modulo04/src/App.js
+++ modulo04/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
 
-import manAtLobby from './assets/manAtLobby.png'
+import TechList from './components/TechList'
 
 function App(){
-  return <img width="200" src={manAtLobby} />
+  return <TechList />
 }
 
 export default App;
