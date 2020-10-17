e235bc1c6c5b8872a6af0f0f4c38b9e0a034c784
vitor <vytorfelix2@gmail.com>
2020-10-17 10:25:56 -0300

    feat: Page Main style (module05)

--- modulo05/src/pages/Main/index.js
+++ modulo05/src/pages/Main/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { FaGitAlt, FaPlus } from 'react-icons/fa';
+
+import { Container, Form, SubmitButton } from './styles';
 
 export default function Main() {
-  return <h1>Main</h1>;
+  return (
+    <Container>
+      <h1>
+        <FaGitAlt />
+        Repositórios
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar repositório" />
+
+        <SubmitButton disabled>
+          <FaPlus color="#FFF" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
