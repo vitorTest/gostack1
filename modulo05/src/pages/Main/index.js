49890698089ef611f3a024302b9d6cf0faf8e057
vitor <vytorfelix2@gmail.com>
2020-10-19 01:16:38 -0300

    feat: add repository page

--- modulo05/src/pages/Main/index.js
+++ modulo05/src/pages/Main/index.js
@@ -3,8 +3,9 @@ import { FaGitAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 import api from '../../services/api';
+import Container from '../../components/Container';
 
-import { Container, Form, SubmitButton, List } from './styles';
+import { Form, SubmitButton, List } from './styles';
 
 export default class Main extends Component {
   state = {
--- modulo05/src/pages/Repository/index.js
+++ modulo05/src/pages/Repository/index.js
@@ -1,7 +1,78 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-// import { Container } from './styles';
+import api from '../../services/api';
+import Container from '../../components/Container';
 
-export default function Repository({ match }) {
-  return <h1>Repository: {decodeURIComponent(match.params.repository)} </h1>;
+import { IssueList, Loading, Owner } from './styles';
+
+export default class Repository extends Component {
+  static propTypes = {
+    match: PropTypes.shape({
+      params: PropTypes.shape({
+        repository: PropTypes.string,
+      }),
+    }).isRequired,
+  };
+
+  state = {
+    repository: {},
+    issues: [],
+    loading: true,
+  };
+
+  async componentDidMount() {
+    const { match } = this.props;
+
+    const repoName = decodeURIComponent(match.params.repository);
+
+    const [repository, issues] = await Promise.all([
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`, {
+        params: {
+          state: 'open',
+          per_page: 5,
+        },
+      }),
+    ]);
+
+    this.setState({ repository });
+    this.setState({ issues });
+
+    if (issues) {
+      this.setState({ loading: false });
+    }
+
+    console.log(repository);
+    console.log(issues);
+  }
+
+  render() {
+    const { repository, issues, loading } = this.state;
+
+    if (loading) {
+      return <Loading>Loading</Loading>;
+    }
+
+    return (
+      <Container>
+        <Owner>
+          <Link to="/">Return to repositories list</Link>
+          <img
+            src={repository.data.owner.avatar_url}
+            alt={repository.data.owner.login}
+          />
+          <h1>{repository.data.name}</h1>
+          <p>{repository.data.description}</p>
+        </Owner>
+
+        <IssueList>
+          {issues.data.map((issue) => {
+            <li key={issue}>{issue}</li>;
+          })}
+        </IssueList>
+      </Container>
+    );
+  }
 }
