e340c508577b663f7cc6f6d2d939859f576ff59c
vitor <vytorfelix2@gmail.com>
2020-10-20 10:13:26 -0300

    feat: module05 completed

--- modulo05/src/pages/Repository/index.js
+++ modulo05/src/pages/Repository/index.js
@@ -68,9 +68,20 @@ export default class Repository extends Component {
         </Owner>
 
         <IssueList>
-          {issues.data.map((issue) => {
-            <li key={issue}>{issue}</li>;
-          })}
+          {issues.data.map((issue) => (
+            <li key={String(issue.id)}>
+              <img src={issue.user.avatar_url} alt={issue.user.login} />
+              <div>
+                <strong>
+                  <a href={issue.html_url}>{issue.title}</a>
+                  {issue.labels.map((label) => (
+                    <span key={String(label.id)}>{label.name}</span>
+                  ))}
+                </strong>
+                <p>{issue.user.login}</p>
+              </div>
+            </li>
+          ))}
         </IssueList>
       </Container>
     );
