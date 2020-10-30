58ff0ab8f487ba429a595f64501f3c81e9894706
vitor <vytorfelix2@gmail.com>
2020-10-30 10:50:08 -0300

    feat: add routes navigation

--- module06/src/index.js
+++ module06/src/index.js
@@ -1,25 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
 
 import './config/ReactotronConfig';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-});
-
-// console.tron.log('Hello World');
-console.tron.log('Hello World');
+import Routes from './routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>You're going to be a millionare</Text>
-      <Text style={styles.welcome}>Soon</Text>
-    </View>
-  );
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1"/>
+      <Routes />
+    </>);
 }
