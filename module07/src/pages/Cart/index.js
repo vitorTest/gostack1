51903b67f22d0f09348908b392a7e7a6f7e3fd73
dev <vytorfelix2@gmail.com>
2020-12-26 17:08:36 -0300

    feat: add style to home

--- module07/src/pages/Cart/index.js
+++ module07/src/pages/Cart/index.js
@@ -1,67 +1,68 @@
 import React from 'react';
-import {
-  MdRemoveCircleOutline,
-  MdAddCircleOutline,
-  MdDelete,
-} from 'react-icons/md';
+// import {
+//   MdRemoveCircleOutline,
+//   MdAddCircleOutline,
+//   MdDelete,
+// } from 'react-icons/md';
 
-import { Container, ProductTable, Total } from './styles';
+// import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-  return (
-    <Container>
-      <ProductTable>
-        <thead>
-          <tr>
-            <th> </th>
-            <th>PRODUCT NAME</th>
-            <th>QUANTITY</th>
-            <th>SUB-TOTAL</th>
-            <th> </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>
-              <img
-                src="https://pbs.twimg.com/media/EbzjpdTUEAEkcA_?format=jpg&name=small"
-                alt="Tênis"
-              />
-            </td>
-            <td>
-              <strong>PRODUCT TITLE</strong>
-              <span>200,00</span>
-            </td>
-            <td>
-              <div>
-                <button type="button">
-                  <MdRemoveCircleOutline size={20} color="#7159c1" />
-                </button>
-                <input type="number" readOnly value={1} />
-                <button type="button">
-                  <MdAddCircleOutline size={20} color="#7159c1" />
-                </button>
-              </div>
-            </td>
-            <td>
-              <strong>R$ 400,00</strong>
-            </td>
-            <td>
-              <button type="button">
-                <MdDelete size={20} color="#7159c1" />
-              </button>
-            </td>
-          </tr>
-        </tbody>
-      </ProductTable>
+  return <h1>Cart</h1>;
+  // return (
+  //   <Container>
+  //     <ProductTable>
+  //       <thead>
+  //         <tr>
+  //           <th> </th>
+  //           <th>PRODUCT NAME</th>
+  //           <th>QUANTITY</th>
+  //           <th>SUB-TOTAL</th>
+  //           <th> </th>
+  //         </tr>
+  //       </thead>
+  //       <tbody>
+  //         <tr>
+  //           <td>
+  //             <img
+  //               src="https://pbs.twimg.com/media/EbzjpdTUEAEkcA_?format=jpg&name=small"
+  //               alt="Tênis"
+  //             />
+  //           </td>
+  //           <td>
+  //             <strong>PRODUCT TITLE</strong>
+  //             <span>200,00</span>
+  //           </td>
+  //           <td>
+  //             <div>
+  //               <button type="button">
+  //                 <MdRemoveCircleOutline size={20} color="#7159c1" />
+  //               </button>
+  //               <input type="number" readOnly value={1} />
+  //               <button type="button">
+  //                 <MdAddCircleOutline size={20} color="#7159c1" />
+  //               </button>
+  //             </div>
+  //           </td>
+  //           <td>
+  //             <strong>R$ 400,00</strong>
+  //           </td>
+  //           <td>
+  //             <button type="button">
+  //               <MdDelete size={20} color="#7159c1" />
+  //             </button>
+  //           </td>
+  //         </tr>
+  //       </tbody>
+  //     </ProductTable>
 
-      <footer>
-        <button type="button">Submit Order</button>
-        <Total>
-          <span>TOTAL</span>
-          <strong>200,00</strong>
-        </Total>
-      </footer>
-    </Container>
-  );
+  //     <footer>
+  //       <button type="button">Submit Order</button>
+  //       <Total>
+  //         <span>TOTAL</span>
+  //         <strong>200,00</strong>
+  //       </Total>
+  //     </footer>
+  //   </Container>
+  // );
 }
--- module07/src/pages/Home/index.js
+++ module07/src/pages/Home/index.js
@@ -1,5 +1,107 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
+
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-globe-se-814-masculino/79/D22-3837-879/D22-3837-879_zoom2.jpg?ts=1594835744&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Cool shoes</strong>
+        <span>R$ 192,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>Add to the cart</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-globe-se-814-masculino/79/D22-3837-879/D22-3837-879_zoom2.jpg?ts=1594835744&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Cool shoes</strong>
+        <span>R$ 192,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>Add to the cart</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-globe-se-814-masculino/79/D22-3837-879/D22-3837-879_zoom2.jpg?ts=1594835744&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Cool shoes</strong>
+        <span>R$ 192,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>Add to the cart</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-globe-se-814-masculino/79/D22-3837-879/D22-3837-879_zoom2.jpg?ts=1594835744&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Cool shoes</strong>
+        <span>R$ 192,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>Add to the cart</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-globe-se-814-masculino/79/D22-3837-879/D22-3837-879_zoom2.jpg?ts=1594835744&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Cool shoes</strong>
+        <span>R$ 192,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>Add to the cart</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-globe-se-814-masculino/79/D22-3837-879/D22-3837-879_zoom2.jpg?ts=1594835744&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Cool shoes</strong>
+        <span>R$ 192,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>Add to the cart</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
