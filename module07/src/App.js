0f6fab164adc8211ffa32f7d681439a7ed397fdb
dev <vytorfelix2@gmail.com>
2020-12-29 23:55:29 -0300

    feat: Add quantity of itens into the cart with reduz ...redux

--- module07/src/App.js
+++ module07/src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import GLobalStyle from './styles/global';
 import Header from './components/Header';
 import Routes from './routes';
 
+import store from './store';
+
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <GLobalStyle />
-      <Routes />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <GLobalStyle />
+        <Routes />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
--- module07/src/pages/Home/index.js
+++ module07/src/pages/Home/index.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
 import { MdAddShoppingCart } from 'react-icons/md';
 import { formatPrice } from '../../util/format';
 import api from '../../services/api';
 
 import { ProductList } from './styles';
 
-export default class Home extends Component {
+class Home extends Component {
   constructor(props) {
     super(props);
     this.state = { products: [] };
@@ -22,6 +25,15 @@ export default class Home extends Component {
     this.setState({ products: data });
   }
 
+  handleAddProduct = (product) => {
+    const { dispatch } = this.props;
+
+    dispatch({
+      type: 'ADD_TO_CART',
+      product,
+    });
+  };
+
   render() {
     const { products } = this.state;
 
@@ -33,7 +45,10 @@ export default class Home extends Component {
             <strong>{product.title}</strong>
             <span>{product.priceFormatted}</span>
 
-            <button type="button">
+            <button
+              type="button"
+              onClick={() => this.handleAddProduct(product)}
+            >
               <div>
                 <MdAddShoppingCart size={16} color="#FFF" /> 3
               </div>
@@ -46,3 +61,10 @@ export default class Home extends Component {
     );
   }
 }
+
+Home.propTypes = {
+  // dispatch: PropTypes.object,
+  dispatch: PropTypes.number.isRequired,
+};
+
+export default connect()(Home);
