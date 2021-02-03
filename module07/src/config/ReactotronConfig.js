d6f57884311503e24428658f337639e5b57847a4
dev <vytorfelix2@gmail.com>
2021-02-03 01:02:06 -0300

    chore: Configure Reactotron to it work with redux-saga

--- module07/src/config/ReactotronConfig.js
+++ module07/src/config/ReactotronConfig.js
@@ -1,5 +1,6 @@
 import Reactotron from 'reactotron-react-js';
 import { reactotronRedux } from 'reactotron-redux';
+import reactotronSaga from 'reactotron-redux-saga';
 
 if (process.env.NODE_ENV === 'development') {
   const tron = Reactotron.configure({
@@ -7,6 +8,7 @@ if (process.env.NODE_ENV === 'development') {
     host: '192.168.15.8',
   })
     .use(reactotronRedux())
+    .use(reactotronSaga())
     .connect();
 
   tron.clear();
