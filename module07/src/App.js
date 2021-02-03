c807d424cb8c15962359eb7753d2555754a238c1
dev <vytorfelix2@gmail.com>
2021-02-03 13:17:15 -0300

    feat: Finishing module/store cart behaviour

--- module07/src/App.js
+++ module07/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+// import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
 
 import './config/ReactotronConfig';
 
@@ -8,15 +10,19 @@ import GLobalStyle from './styles/global';
 import Header from './components/Header';
 import Routes from './routes';
 
+// import history from './services/history';
 import store from './store';
 
 function App() {
   return (
     <Provider store={store}>
       <BrowserRouter>
+        {/* <Router history={history}> */}
         <Header />
         <GLobalStyle />
+        <ToastContainer autoClose={3000} />
         <Routes />
+        {/* </Router> */}
       </BrowserRouter>
     </Provider>
   );
